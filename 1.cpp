4c921efa2f81c75427bb4bef6604d8cb285a5219
soam69 <sohpankhade@gmail.com>
2025-01-02 23:23:06 +0530

    Problem 1

--- 1.cpp
+++ 1.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include<vector>
 using namespace std;
 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for ( int i = 0; i < n; i++){
+            int temp = target - nums[i];
+            for (int j = i+1; j < n; j++){
+                if (nums[j]==temp){
+                    return {i,j};
+                }
+            }
+        }
+        return {};
+    }
+};
 
 int main(){
     
