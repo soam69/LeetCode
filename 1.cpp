2ca099818de3c90783c7d170b84c4ef71db40696
soam69 <sohpankhade@gmail.com>
2025-02-10 20:50:26 +0530

    10 Feb

--- 1.cpp
+++ 1.cpp
@@ -14,7 +14,7 @@ public:
                 }
             }
         }
-        return {};
+        return {}; 
     }
 };
 
---
+++ 3174.cpp
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/clear-digits/description/?envType=daily-question&envId=2025-02-10
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+    public:
+        string clearDigits(string s) {
+            string ans = "";
+            int n = s.length();
+            char st[n];
+            int top = -1;
+            for(int i = 0; i < n; i++){
+                if(s[i] >= '0' && s[i] <= '9'){
+                    top--;
+                }
+                else{
+                    st[++top] = s[i];
+                }
+            }
+            if(top < 0){
+                return ans;
+            }
+            else{
+                for(int i = 0; i <= top; i++){
+                    ans += st[i];
+                }
+            }
+            return ans;
+        }
+    };
+
+int main(){
+    
+    return 0;
+}
