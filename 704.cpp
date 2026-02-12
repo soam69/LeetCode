21f29165793d0fc5488296a0f9b38b78fa742418
soam69 <sohpankhade@gmail.com>
2026-02-12 21:19:32 +0530

    12 Feb 26

---
+++ 704.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/binary-search/description/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size()-1;
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < target){
+                left = mid + 1;
+            }
+            else right = mid-1;
+        }
+        return -1;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
