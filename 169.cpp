304a32d2b3b835ef431730c686cf6afc93d1725d
soam69 <sohpankhade@gmail.com>
2026-02-16 01:16:50 +0530

    16 Feb 26

---
+++ 169.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/majority-element/description/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int size = nums.size();
+        map<int,int> hash;
+        for(int i : nums){
+            hash[i]++;
+            if(hash[i] > size/2) return i;
+        }
+        return 0;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
