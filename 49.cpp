fca69a6421fd388c1cfc07425569843881e69393
soam69 <sohpankhade@gmail.com>
2026-03-26 15:44:50 +0530

    26 Mar 26

---
+++ 49.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/group-anagrams/description/
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<string, vector<string>> mp;
+        
+        for(string str : strs){
+            string s = str;
+            sort(s.begin(),s.end());
+            mp[s].push_back(str);
+        }
+        vector<vector<string>> ans;
+        for(auto it : mp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
