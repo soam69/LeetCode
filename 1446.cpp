fc9cd5a91b98303deafc5c7fff5af4adb471068e
soam69 <sohpankhade@gmail.com>
2026-05-15 16:38:26 +0530

    19 May 26

---
+++ 1446.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/consecutive-characters/description/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxPower(string s) {
+        int count = 0;
+        char curr = s[0];
+        int maxi = 1;
+        for(char c : s){
+            if(c == curr){
+                count ++;
+            }
+            else{
+                curr = c;
+                maxi = max(maxi, count);
+                count = 1;
+            }
+        }
+        return max(maxi,count);
+    }
+};
+
+int main(){
+    
+    return 0;
+}
