403c06d6c00573e20c6b778ab26fac290a3b38fb
soam69 <sohpankhade@gmail.com>
2026-06-26 16:25:08 +0530

    26 June 26

---
+++ 2769.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/find-the-maximum-achievable-number/description/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        return num + t*2;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
